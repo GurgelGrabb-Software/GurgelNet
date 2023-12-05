b65b7b6ef226e6bf97f011e2c3f8bf58215d163e
OstGeneralen <kasper.esbjornsson@gmail.com>
2023-12-05 12:48:17 +0000

    Remove unused net message type internal

--- Include/GurgelNet/Messages/NetMessageTypes.h
+++ Include/GurgelNet/Messages/NetMessageTypes.h
@@ -11,10 +11,8 @@
 
 enum ENetMsgCategory : uint8_t
 {
-	ENetMsgCategory_Internal	= 0x00, // TODO: Remove this :)
-
-	ENetMsgCategory_Connection,	// Internal category for handling connections (handshakes, approval etc.)
-	ENetMsgCategory_Object,		// Internal category for handling objects (spawns, despawns etc.)
+	ENetMsgCategory_Connection,		// Internal category for handling connections (handshakes, approval etc.)
+	ENetMsgCategory_Object,			// Internal category for handling objects (spawns, despawns etc.)
 
 	ENetMsgCategory_Custom,			// This should be your start when creating custom message types
 };
