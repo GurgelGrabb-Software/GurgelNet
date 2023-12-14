e8404a0b6318ed04c802546560f0c35b67009338
OstGeneralen <kasper.esbjornsson@gmail.com>
2023-12-14 10:43:20 +0000

    Fix reading incorrect owner mask client side

--- Src/Include/NetLayerMessages/ObjectMessages.h
+++ Src/Include/NetLayerMessages/ObjectMessages.h
@@ -55,7 +55,7 @@ public:
 	{
 		serializer.Write(objectHandle->objectPtr->GetNetObjectID());
 		serializer.Write(objectHandle->objectPtr->GetNetTypeID());
-		serializer.Write(objectHandle->objectPtr->GetOwnerMask());
+		serializer.Write(ownerID);
 		objectHandle->objectPtr->WriteSpawnData(serializer);
 	}
 
